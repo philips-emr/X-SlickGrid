philips-emr/X-SlickGrid

commit 9f5c6cd4fad3d24941da6d9b84f3f9d6357e34b5

Fixing fullWidthRows option

slick.grid.js
@@ -710,7 +710,9 @@
         $headerR.width(headersWidthR);
 
         if (hasFrozenColumns()) {
-          $canvasTopR.width(canvasWidthR);
+          if (options.fullWidthRows) {
+            $canvasTopR.css("min-width", "100%");
+          }
 
           $paneHeaderL.width(canvasWidthL);
           $paneHeaderR.css('left', canvasWidthL);
@@ -763,6 +765,10 @@
           if (hasFrozenRows) {
             $viewportBottomL.width('100%');
             $canvasBottomL.width(canvasWidthL);
+          }
+
+          if (options.fullWidthRows) {
+            $canvasTopL.css("min-width", "100%");
           }
         }
 

commit d9ad4dc73d1a6a2c9858ae007e5a5b339d5eb235

Fixing column sizing bug introduced by 9f5c6cd4fad3d24941da6d9b84f3f9d6357e34b5

slick.grid.js
@@ -710,6 +710,7 @@
         $headerR.width(headersWidthR);
 
         if (hasFrozenColumns()) {
+          $canvasTopR.width(canvasWidthR);
           if (options.fullWidthRows) {
             $canvasTopR.css("min-width", "100%");
           }